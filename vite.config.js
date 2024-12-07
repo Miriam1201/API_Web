a12540ee28897d61c0690d06672353f50fd2fa20
Miriam1201 <miriam@growersgo.com>
2024-12-07 23:03:38 +0100

    FixVite

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,12 @@ export default defineConfig({
             '@': path.resolve(__dirname, './resources/js'),
         },
     },
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                app: './resources/js/app.js',
+            },
+        },
+    },
 });
